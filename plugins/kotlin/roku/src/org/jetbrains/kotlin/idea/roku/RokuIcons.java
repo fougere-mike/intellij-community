35bb459a2989ebeb4f329a4be18ac120d16be817
Mike Fougere <fougere.mike@gmail.com>
2026-05-08 13:18:04 -0400

    icons: bring custom Roku/BRS SVG artwork from kotlin-roku-intellij

--- plugins/kotlin/roku/src/org/jetbrains/kotlin/idea/roku/RokuIcons.java
+++ plugins/kotlin/roku/src/org/jetbrains/kotlin/idea/roku/RokuIcons.java
@@ -2,32 +2,36 @@
 package org.jetbrains.kotlin.idea.roku;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.ui.IconManager;
+import com.intellij.openapi.util.IconLoader;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-/**
- * Icons for Roku IDE features.
- * Note: Until custom SVG icons are created, we use placeholder icons from AllIcons.
- */
+/** Icons for Roku IDE features. */
 public final class RokuIcons {
-    private RokuIcons() {
-    }
+    private RokuIcons() {}
 
-    // Placeholder icons - replace with custom Roku icons when available
-    /** 16x16 icon for Roku device */
-    public static final @NotNull Icon ROKU = AllIcons.RunConfigurations.Remote;
+    /** 16x16 icon for Roku device. */
+    public static final @NotNull Icon ROKU = IconLoader.getIcon("/icons/roku_16.svg", RokuIcons.class);
 
-    /** 13x13 icon for tool windows */
-    public static final @NotNull Icon ROKU_13 = AllIcons.RunConfigurations.Remote;
+    /** 13x13 icon for tool windows. */
+    public static final @NotNull Icon ROKU_13 = IconLoader.getIcon("/icons/roku_13.svg", RokuIcons.class);
 
-    /** 16x16 icon for connected Roku device */
+    /** 16x16 icon for connected Roku device. */
     public static final @NotNull Icon ROKU_CONNECTED = AllIcons.RunConfigurations.TestState.Green2;
 
-    /** 16x16 icon for disconnected Roku device */
+    /** 16x16 icon for disconnected Roku device. */
     public static final @NotNull Icon ROKU_DISCONNECTED = AllIcons.RunConfigurations.TestState.Red2;
 
-    /** 16x16 icon for Roku device requiring authentication */
+    /** 16x16 icon for Roku device requiring authentication. */
     public static final @NotNull Icon ROKU_AUTH_REQUIRED = AllIcons.RunConfigurations.TestState.Yellow2;
+
+    /** 13x13 icon for the Roku Log tool window. */
+    public static final @NotNull Icon DEVICE_LOG = IconLoader.getIcon("/icons/roku_log_13.svg", RokuIcons.class);
+
+    /** 13x13 icon for the Roku test results panel. */
+    public static final @NotNull Icon TEST_RESULTS = IconLoader.getIcon("/icons/roku_test_13.svg", RokuIcons.class);
+
+    /** 16x16 icon for .brs source files. */
+    public static final @NotNull Icon BRS_FILE = IconLoader.getIcon("/icons/brs_file_16.svg", RokuIcons.class);
 }
